7faddfed3a41313ee4fee5ddf35b1d0ebc30a4ac
Vinay Kumar Nutenki <70644462+nutenkivinaykumar@users.noreply.github.com>
2023-06-13 21:07:33 +0530

    Update 1.cpp

--- GFG_CIP/3_Array_String_Pointer&Reference/vid_13_RangeBasedForloopsAndReferences/1.cpp
+++ GFG_CIP/3_Array_String_Pointer&Reference/vid_13_RangeBasedForloopsAndReferences/1.cpp
@@ -21,3 +21,26 @@ int main()
         cout << s << '\n';
     return 0;
 }
+
+/*
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {10, 20, 30};
+    
+    for(int i = 0; i < 3; i++){
+        arr[i] = i; 
+    }
+    
+    for(int i = 0; i < 3; i++){
+        cout << arr[i] << endl; 
+    }
+    return 0;
+}
+
+
+*/
